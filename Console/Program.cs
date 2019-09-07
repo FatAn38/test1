b828a2f7b1b97646a5e1afdbc1c138b65905a8b1
FatAn38 <fath@list.ru>
2019-09-07 21:23:41 +0800

    Initial commit

---
+++ Console/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Library1;
+
+namespace Console1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine(Library1.Class1.GetHello());
+			Console.ReadKey();
+		}
+	}
+}
